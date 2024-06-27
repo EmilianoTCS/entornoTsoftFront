33b6cd24ea38f748033980fb6949f9769e502ec3
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2024-06-27 18:37:44 -0300

    before deploy entrno tsoft

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -5,6 +5,7 @@ export default function LoginService({ username, password }) {
   return fetch(baseURL, {
     method: "POST",
     body: JSON.stringify({ username, password }),
+    "Content-Type": "application/json",
   })
     .then((response) => {
       if (!response) throw new Error("Fallo en la conexion");
