495862b483df42fb434c1aa2c7093fa98b07e666
EmilianoTCS <emi.vega84@gmail.com>
2024-06-28 10:54:38 -0300

    hot fix get data service

--- src/services/GetDataService.js
+++ src/services/GetDataService.js
@@ -1,6 +1,6 @@
 export default function getDataService(url, operationUrl) {
   const baseURL = `https://entornotsoft-backend.000webhostapp.com/${url}?${operationUrl}`;
-ft
+
   return fetch(baseURL)
     .then((res) => res.json())
     .then((response) => {
